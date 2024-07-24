92fdfc3361463e2851d0a0d27c682468dd52e3ae
João Victor <hauntermonster68@gmail.com>
2024-07-23 23:29:17 -0300

    Atualização do projeto para continuidade futura

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
-import { Routes } from '@angular/router';
+import { RedirectCommand, Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'courses', pathMatch: 'full' },
+  { path: 'courses',
+    loadChildren: () => import('./courses/courses.module').then(m => m.CoursesModule)
+  }
+];
